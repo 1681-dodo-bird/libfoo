9af2971bc8c017ba3ee58eaa26cdd4d0a7a5be14
dodo <1681.dodo.bird@gmail.com>
2017-07-24 14:15:33 +0900

    Example: Struct to byte array

--- libfoo.go
+++ libfoo.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"C"
+	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
+
+type Hoge struct {
+	Foo  int64
+	Bar  float64
+	Data [256]byte
+}
+
+//export HogeHoge
+func HogeHoge() {
+	h := Hoge{Foo: 33, Bar: 44.333}
+	copy(h.Data[:], "Hello World")
+
+	buf := &bytes.Buffer{}
+	err := binary.Write(buf, binary.BigEndian, h)
+	if err != nil {
+		panic(err)
+	}
+
+	h2 := Hoge{}
+	err = binary.Read(buf, binary.BigEndian, &h2)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%d %f %s\n", h2.Foo, h2.Bar, h2.Data)
+}
 
 //export Fooooo
 func Fooooo() int64 {
@@ -25,4 +52,5 @@ func Fooooo() int64 {
 
 func main() {
 	fmt.Println("vim-go")
+	HogeHoge()
 }
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 import ctypes
 lib = ctypes.cdll.LoadLibrary('./libfoo-linux-amd64.so')
 lib.Fooooo()
+lib.HogeHoge()
